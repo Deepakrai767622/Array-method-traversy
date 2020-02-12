34391a67e08626c3edbd8c6487d54ed26221f56d
Deepak Rai <Deepakrai767622@gmail.com>
2020-02-12 11:55:23 +0530

    End

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const addUserBtn = document.getElementById('add-user');
 const doubleBtn = document.getElementById('double');
 const showMillionairesBtn = document.getElementById('show-millionaires');
 const sortBtn = document.getElementById('sort');
-const calculateWealthBtn = document.getElementById('calculate');
+const calculateWealthBtn = document.getElementById('calculate-wealth');
 
 let data = [];
 
@@ -19,11 +19,43 @@ async function getRandomUser() {
     const user = data.results[0];
     const newUser = {
         name: `${user.name.first} ${user.name.last}`,
-        money: Math.floor(Math.random() * 10000000)
+        money: Math.floor(Math.random() * 1000000)
     }
 
     addData(newUser)
 
+}
+
+// Double Everyone money
+function doubleMoney() {
+    data = data.map((user) => {
+        return { ...user, money: user.money * 2 };
+    });
+
+    updateDOM();
+}
+
+// Sort users by richest
+function sortByRichest() {
+    data.sort((a, b) => b.money - a.money);
+
+    updateDOM();
+}
+
+// Filter only millionaires
+function showMillionaires() {
+    data = data.filter(user => user.money > 1000000);
+
+    updateDOM();
+}
+
+// calculate the total wealth
+function calculateWealth() {
+    const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+
+    const wealthEl = document.createElement('div')
+    wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong> </h3>`
+    main.appendChild(wealthEl);
 }
 
 
@@ -41,16 +73,22 @@ function updateDOM(providedData = data) {
     //clear the main div
     main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
 
-
     providedData.forEach(item => {
         const element = document.createElement('div')
         element.classList.add('person');
-        element.innerHTML = `<strong>${item.name}</strong> ${item.money}`;
+        element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(item.money)}`;
         main.appendChild(element);
     });
 }
 
-// Formate number as money
-function FormatMoney() {
-
+// Formate number as money -  https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string //
+function formatMoney(number) {
+    return '$ ' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
 }
+
+// Event Listeners
+addUserBtn.addEventListener('click', getRandomUser);
+doubleBtn.addEventListener('click', doubleMoney);
+sortBtn.addEventListener('click', sortByRichest);
+showMillionairesBtn.addEventListener('click', showMillionaires);
+calculateWealthBtn.addEventListener('click', calculateWealth);
